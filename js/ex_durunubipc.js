f2509d13a8ae0df2cb161eed655b7e6655473ebc
kwonnaram <111223747+kwonnaram@users.noreply.github.com>
2022-10-14 01:01:05 +0900

    수정

---
+++ js/ex_durunubipc.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $(".sec01 .left").slick();
+});
---
+++ js/slick_slide.js
@@ -0,0 +1,9 @@
+// 기본형
+$(document).ready(function(){
+    $(".slide").slick();
+});
+
+// 함축형
+$(function(){
+    $(".slide").slick();
+});
