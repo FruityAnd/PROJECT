efe47a64f34808d09833d75fdc106136cf35b480
kwonnaram <111223747+kwonnaram@users.noreply.github.com>
2022-10-19 04:29:24 +0900

    업로드

--- js/01_cancer_home.js
+++ js/01_cancer_home.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
     // slide 
     $(".sec01 .slickBanner").slick({
         dots: true,
+        arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3500,
+        autoplaySpeed: 1500
     });
 
     // button: scrollTop
---
+++ js/04_cancer_sub3.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // button: scrollTop
+    $(".pageTop").click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000)
+    });
+});
