4aad385340501121e824d1f56831ca4815a0adbf
Fruity <111223747+FruityAnd@users.noreply.github.com>
2022-11-22 01:52:36 +0000

    탭버튼 수정

--- js/cancer_sub2.js
+++ js/cancer_sub2.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //병기에 따른 대장암의 분류
     //tabButton
-    $(".tabH .tabButton").click(function () {
+    $(".tabH .tab-button").click(function () {
         var tabindex = $(this).index();
         $(this).addClass('on').siblings().removeClass('on');
         $(".tabH .tabBox").eq(tabindex).addClass('on').siblings().removeClass('on');
